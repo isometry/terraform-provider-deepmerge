4c409b73b454877a51efd66fb6104d05906e0356
Robin Breathe <robin@isometry.net>
2024-11-10 17:05:04 +0100

    docs: improve example usage

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -7,3 +7,22 @@ terraform {
 }
 
 provider "deepmerge" {}
+
+locals {
+  map1 = {
+    a = {
+      x = [1, 2, 3]
+      y = false
+    }
+    b = {
+      s = "hello, world"
+      n = 17
+    }
+  }
+  map2 = {
+    a = { x = [4, 5, 6] }
+    b = { n = 42 }
+  }
+
+  merged = provider::deepmerge::mergo(local.map1, local.map2)
+}
